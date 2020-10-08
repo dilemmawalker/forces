4c87941c55fdba8db4884e563689043a0d1abf3a
yash handa <yashhanda500@gmail.com>
2020-10-08 20:53:39 +0530

    update

---
+++ forces664/prob1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class prob1{
+    public static Scanner scn=new Scanner(System.in);
+    public static void main(String[]args){
+        int t=scn.nextInt();
+    }
+}
