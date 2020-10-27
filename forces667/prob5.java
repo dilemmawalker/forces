5eef36426384bdfbe8477dc0caa12d39e301b34a
yash handa <yashhanda500@gmail.com>
2020-10-27 19:58:47 +0530

    update

---
+++ forces667/prob5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class prob5{
+    public static Scanner scn = new Scanner(new InputStreamReader(System.in));
+    public static void main(String[]args){
+        int t=scn.nextInt();
+        while(t-->0){
+            int 
+        }
+    }
+}
