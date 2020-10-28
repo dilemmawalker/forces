3a6e161a5ef76b252a3957aaef4f89482eb7a4e4
yash handa <yashhanda500@gmail.com>
2020-10-28 14:29:14 +0530

    update

---
+++ round97/prob3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class prob3{
+    public static Scanner scn=new Scanner(new InputStreamReader(System.in));
+    public static void main(String[]args){
+        int t=scn.nextInt();
+        while(t-->0){
+            
+        }
+    }
+}
