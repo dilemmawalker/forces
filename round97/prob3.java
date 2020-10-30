99f0a7d7e1c5d42b5636f0574859f878415865c4
yash handa <yashhanda500@gmail.com>
2020-10-30 22:06:47 +0530

    update

--- round97/prob3.java
+++ round97/prob3.java
@@ -11,33 +11,35 @@ public class prob3{
                 int a=scn.nextInt();
                 arr[i]=a;
             }
-            int[]dp=new int[n];
+            int[][]dp=new int[n][200];
             int a=solve(arr,new boolean[200],n-1,dp);
             System.out.println(a);
         }
     }
-    public static int solve(int[]arr,boolean[]ar,int idx,int []dp){
+    public static int solve(int[]arr,boolean[]ar,int idx,int [][]dp){
         if(idx < 0){
             return 0;
         }
-        if(dp[idx]!=0)
-        return dp[idx];
-        int c=1000000; 
+        // if(dp[idx]!=0)
+        // return dp[idx];
+        int c=1000000;
         int a=0;
         int ii=0;
         for(int i=0;i<ar.length;i++){
             if(!ar[i]){
-                // ar[i]=true;
+                ar[i]=true;
                 int calc=i+1-arr[idx];
-                a=solve(arr,ar,idx-1,dp)+ (calc>0?calc:-calc);
+                if(dp[idx][i]!=0)
+                a=dp[idx][i];
+                else
+                a=solve(arr,ar,idx-1,dp)+ (calc>=0?calc:-calc);
                 if(a<c){
                 c=a;
-                ii=i;
                 }
-                // ar[i]=false;
+                ar[i]=false;
+                dp[idx][i]=c;
             }
         }
-        ar[ii]=true;
-        return dp[idx]=c;
+        return c;
     }
 }
