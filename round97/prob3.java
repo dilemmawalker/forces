f3c3ef82282d749ca249c4d070325ac932a28ff8
yash handa <yashhanda500@gmail.com>
2020-10-31 12:30:44 +0530

    updatae

--- round97/prob3.java
+++ round97/prob3.java
@@ -12,7 +12,13 @@ public class prob3{
                 arr[i]=a;
             }
             int[][]dp=new int[n][200];
-            int a=solve(arr,new boolean[200],0,dp);
+            for(int i=0;i<n;i++){
+                for(int j=0;j<200;j++){
+                    dp[i][j]=-1;
+                }
+            }
+            Arrays.sort(arr);
+            int a=solve(arr,new boolean[200],0,dp,0);
             System.out.println(a);
             // display(dp);
         }
@@ -22,33 +28,26 @@ public class prob3{
         int m=arr[0].length;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                System.out.print(arr[i][j]);
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
     }
-    public static int solve(int[]arr,boolean[]ar,int idx,int [][]dp){
+    public static int solve(int[]arr,boolean[]ar,int idx,int [][]dp,int iii){
         if(idx == arr.length){
             return 0;
         }
-        // if(dp[idx]!=0)
-        // return dp[idx];
-        int c=1000000;
+        if(iii==200)
+        return 100000000;
+        if(dp[idx][iii]!=-1)
+        return dp[idx][iii];
+        int c=100000000;
         int a=0;
-        int ii=0;
-        for(int i=0;i<ar.length;i++){
-            if(!ar[i]){
-                ar[i]=true;
+        for(int i=iii;i<ar.length;i++){
                 int calc=i+1-arr[idx];
-                if(dp[idx][i]!=0)
-                a=dp[idx][i];
-                else
-                a=solve(arr,ar,idx+1,dp)+ (calc>=0?calc:-calc);
+                a=solve(arr,ar,idx+1,dp,i+1)+ (calc>=0?calc:-calc);
                 c=Math.min(a,c);
-                ar[i]=false;
-                dp[idx][i]=c;
             }
-        }
-        return c;
+        return dp[idx][iii]=c;
     }
 }
