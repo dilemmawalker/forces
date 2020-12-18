3206e6cc4a1729fa7d2744f16c3a5000ff5b5890
yash handa <yashhanda500@gmail.com>
2020-12-19 01:23:21 +0530

    update

--- round100/prob1.java
+++ round100/prob1.java
@@ -8,25 +8,24 @@ public static void main(String[]args){
         int min=100000000;
         long count=0l;
         int a=scn.nextInt();
-        min=Math.min(min,a);
         int b=scn.nextInt();
-        min=Math.min(min,b);
         int c=scn.nextInt();
-        min=Math.min(min,c);
         count=a+b+c;
+        min=Math.min(Math.min(a,b),c);
 
         long n=0l;
-         n=count-2*(count/7);
-         long no=0;
-          no=n/7;
-        if(min>=no){
-            if((no*3+(n-no))==n)
+         long no=0l;  
+         no=count/9;               //7th shots fired
+         n=no*6;                     //normal shots fired
+        if(no>0 && min>=no && (3*no+n)==count){
+            // if((no*3+(n-no))==n)
             System.out.println("yes");
-            else
-            System.out.println("no");
+            // else
+            // System.out.println("no");
         }
         else
         System.out.println("no");
+        // System.out.println(n +" "+ no);
     }
 }
 }
--- round100/prob2.java
+++ round100/prob2.java
@@ -1,11 +1,37 @@
 import java.io.*;
 import java.util.*;
-public class prob1{
-public static Scanner scn=new Scanner(System.in);
-public static void main(String[]args){
-    int t=scn.nextInt();
-    for(int i=1;i<=t;i++){
-        System.out.println(i);
+public class prob2{
+    public static Scanner scn=new Scanner(System.in);
+    public static void main(String[]args){
+        int t=scn.nextInt();
+        while(t-->0){
+            int n=scn.nextInt();
+            int[]arr=new int[n];
+            for(int i=0;i<n;i++){
+                int a=scn.nextInt();
+                arr[i]=a;
+            }
+            System.out.print(arr[0]);
+            int p=arr[0];
+            for(int i=1;i<n;i++){
+                int a=arr[i];
+                if(a>=arr[i-1]){
+                    int m=a/p;
+                    int ans1=m*p;
+                    int ans2=m*(p+1);
+                    int ans1=((a-ans1)<=(ans2-a))?ans1:ans2;
+                    p=ans1;
+                    System.out.print(p);
+                }
+                else{
+                    int m=p/a;
+                    int ans1=m*a;
+                    int ans2=m*(a+1);
+                    int ans1=((a-ans1)<=(ans2-a))?ans1:ans2;
+                    p=ans1;
+                    System.out.print(p);
+                }
+            }
+        }
     }
-}
 }
