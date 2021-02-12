65b8bc9d37ddf1caf833a5883c2f499ba49656c4
yash handa <yashhanda500@gmail.com>
2021-02-12 21:26:25 +0530

    update

---
+++ round701/prob1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class prob1{
+    public static Scanner scn=new Scanner(System.in);
+    public static void main(String[]args){
+        int n=scn.nextInt();
+        
+    }
+}
