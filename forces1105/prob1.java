035e4e1f07fa7be1b54a2278301c66264db5a5b5
Yash Handa <yashhanda500@gmail.com>
2026-06-28 20:50:27 +0530

    didn't solve any problem forces

---
+++ forces1105/prob1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class prob1 {
+    public Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
+        int t = scn.nextInt();
+        while(t-->0) {
+            int n = scn.nextInt();
+            int k = scn.nextInt();
+
+            
+        }
+    }
+}
